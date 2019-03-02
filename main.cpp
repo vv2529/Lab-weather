23a71e1bd5000fc642d2afa84e0a573780eacdce
vv2529 <vv02529@gmail.com>
2019-03-02 22:46:11 +0200

    Add task info

--- main.cpp
+++ main.cpp
@@ -5,8 +5,13 @@
 
 using namespace std;
 
+void task() {
+    cout << "This program reads, analyzes and outputs weather data.\n";
+}
+
 int main() {
     copyright();
+    task();
 
     return 0;
 }
