462384d5c2b9f482d0d1f4a47d4bbec7635e411a
vv2529 <vv02529@gmail.com>
2019-03-04 15:25:10 +0200

    Add help

--- main.cpp
+++ main.cpp
@@ -8,10 +8,14 @@ using namespace std;
 void task() {
     cout << "This program reads, analyzes and outputs weather data.\n";
 }
+void help() {
+    cout << "Run this program with arguments:\n> lab.exe <input_file> [-output|-stat <output_file>]*\n\n";
+}
 
 int main() {
     copyright();
     task();
+    help();
 
     return 0;
 }
