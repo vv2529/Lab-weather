94ff9be9cf179e1ce7d31a004e9d4569b09778ca
vv2529 <vv02529@gmail.com>
2019-03-26 02:25:57 +0200

    Add operator overload

--- Date.cpp
+++ Date.cpp
@@ -8,6 +8,31 @@ double Date::gethumidityMin() const noexcept { return humidityMin; }
 double Date::gethumidityMax() const noexcept { return humidityMax; }
 double Date::getdownfallAvg() const noexcept { return downfallAvg; }
 
+bool Date::operator == (const Date& other) const noexcept {
+    return month == other.getMonth()
+        && downfallAvg == other.getdownfallAvg()
+        && year == other.getYear()
+        && day == other.getDay();
+}
+bool Date::operator != (const Date& other) const noexcept {
+    return !(this == other);
+}
+bool Date::operator < (const Date& other) const noexcept {
+    return month < other.getMonth()
+        || downfallAvg < other.getdownfallAvg()
+        || year < other.getYear()
+        || day < other.getDay();
+}
+bool Date::operator <= (const Date& other) const noexcept {
+    return (this < other) || (this == other);
+}
+bool Date::operator > (const Date& other) const noexcept {
+    return !(this <= other);
+}
+bool Date::operator >= (const Date& other) const noexcept {
+    return !(this < other);
+}
+
 std::string Date::Station::getStationCode() const noexcept { return stationCode; }
 int Date::Station::getDownfall() const noexcept { return downfall; }
 double Date::Station::getDayTempMin() const noexcept { return dayTempMin; }
@@ -15,3 +40,24 @@ double Date::Station::getDayTempMax() const noexcept { return dayTempMax; }
 double Date::Station::getDayTempAvg() const noexcept { return dayTempAvg; }
 double Date::Station::getHumidity() const noexcept { return humidity; }
 double Date::Station::getWind() const noexcept { return wind; }
+
+bool Date::Station::operator == (const Station& other) const noexcept {
+    return humidity == other.humidity
+        || stationCode == other.stationCode;
+}
+bool Date::Station::operator != (const Station& other) const noexcept {
+    return !(this == other);
+}
+bool Date::Station::operator < (const Station& other) const noexcept {
+    return humidity < other.humidity
+        || stationCode < other.stationCode;
+}
+bool Date::Station::operator <= (const Station& other) const noexcept {
+    return (this < other) || (this == other);
+}
+bool Date::Station::operator > (const Station& other) const noexcept {
+    return !(this <= other);
+}
+bool Date::Station::operator >= (const Station& other) const noexcept {
+    return !(this < other);
+}
